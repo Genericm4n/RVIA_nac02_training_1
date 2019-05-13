9801fdffccf4667b49bfb4a0cc63e9264e747252
Júlio "genericman" César <47559029+Genericm4n@users.noreply.github.com>
2019-05-13 17:06:38 -0300

    Inclusão do Asset e a criação do Blend Tree (falta configurá-lo apropriadamente)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // definicao de variaveis
+
+    #region variables
+
+    public float velAtual;
+    public float velMax = 2.0f;
+    public float xrl8I;
+    public float xrl8F;
+    public float dxrl8;
+
+    private Animator anime;
+
+    #endregion variables
+
+    private void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+    }
+}
